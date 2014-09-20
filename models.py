7039a433391ca0585ec392f58a724f59b1538e91
Dougal Scott <Dougal.Scott@gmail.com>
2014-09-20 12:03:46 +1000

    Flake8'd Python3'd

--- models.py
+++ models.py
@@ -1,36 +1,38 @@
-#!/opt/local/bin/python
+#!/usr/bin/env python
 
 from volume import Volume
 
-for h in (2,3,4):
+for h in (2, 3, 4):
     for x in range(6):
-        v=Volume(100)
+        v = Volume(100)
         for i in range(100):
             for j in range(h):
-                v.clear((i,50+j,50))
-            for sb in range(-x,x+1):
-                v.clear((i,51,50+sb))
-        print "%d height tunnel with %d continuous side block=%s (Digs %d See %d)" % (h,x,v.score(),v.digs,v.getVisible())
-    print
+                v.clear((i, 50+j, 50))
+            for sb in range(-x, x+1):
+                v.clear((i, 51, 50+sb))
+        print("%d height tunnel with %d continuous side block=%s (Digs %d See %d)" % (h, x, v.score(), v.digs, v.getVisible()))
+    print()
 
     for x in range(6):
-        v=Volume(100)
+        v = Volume(100)
         for i in range(100):
             for j in range(h):
-                v.clear((i,50+j,50))
-            if i%2==0:
-                for sb in range(-x,x+1):
-                    v.clear((i,51,50+sb))
-        print "%d height tunnel with %d alternating side block=%s (Digs %d See %d)" % (h,x,v.score(),v.digs,v.getVisible())
-    print
+                v.clear((i, 50+j, 50))
+            if i % 2 == 0:
+                for sb in range(-x, x+1):
+                    v.clear((i, 51, 50+sb))
+        print("%d height tunnel with %d alternating side block=%s (Digs %d See %d)" % (h, x, v.score(), v.digs, v.getVisible()))
+    print()
 
     for x in range(6):
-        v=Volume(100)
+        v = Volume(100)
         for i in range(100):
             for j in range(h):
-                v.clear((i,50+j,50))
-            if i%3==0:
-                for sb in range(-x,x+1):
-                    v.clear((i,51,50+sb))
-        print "%d height tunnel with %d every third side block=%s (Digs %d See %d)" % (h,x,v.score(),v.digs,v.getVisible())
-    print
+                v.clear((i, 50+j, 50))
+            if i % 3 == 0:
+                for sb in range(-x, x+1):
+                    v.clear((i, 51, 50+sb))
+        print("%d height tunnel with %d every third side block=%s (Digs %d See %d)" % (h, x, v.score(), v.digs, v.getVisible()))
+    print()
+
+# EOF
